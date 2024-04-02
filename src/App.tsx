4a7c3b9ebc56e7517214c4efb119aa8c3c592d03
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2024-04-02 07:52:45 +0700

    feat: setup react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { Routes, Route } from 'react-router-dom'
+import Login from '@/pages/Login'
+import Home from '@/pages/Home'
+
 const App = () => {
 
   return (
     <>
-      <h1>Hello World</h1>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   )
 }
