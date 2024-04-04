eb502c47824d6c967e8e224c6f797a0fb3b6b8ba
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2024-04-04 14:48:45 +0700

    style: formatting with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-import { Routes, Route } from 'react-router-dom'
-import Login from '@/pages/Login'
-import Home from '@/pages/Home'
+import { Routes, Route } from 'react-router-dom';
+import Login from '@/pages/Login';
+import Home from '@/pages/Home';
 
 const App = () => {
-
   return (
-    <>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </>
-  )
-}
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+};
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import { BrowserRouter as Router } from 'react-router-dom'
+import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -9,5 +9,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <Router>
       <App />
     </Router>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
