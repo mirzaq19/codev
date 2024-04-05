80e15821d480648f86b7d31bbb662f910de7432c
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2024-04-05 13:17:14 +0700

    style: fix padding form in login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -43,7 +43,7 @@ const Login = () => {
   };
 
   return (
-    <div className="flex flex-col justify-center items-center max-w-md mx-auto min-h-main">
+    <div className="flex flex-col justify-center items-center max-w-md mx-auto min-h-main px-4 md:px-0">
       <div
         className={cn('max-w-3xl w-full mx-auto', 'bg-white rounded-md p-8')}
       >
