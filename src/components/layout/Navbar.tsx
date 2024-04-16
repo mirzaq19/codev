585a7098ba1c18f950e0d5e1da6db91e1df1b640
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2024-04-16 11:34:00 +0700

    feat: add avatar dropdown to navbar component

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,8 +1,18 @@
 import { Link } from 'react-router-dom';
+import { LogOut } from 'lucide-react';
 import Container from '@/components/layout/Container';
 import { cn } from '@/lib/utils';
 import codevlogo from '@/assets/logo/codev-logo.svg';
-import { Button } from '../ui/button';
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
 type NavbarProps = {
   classname?: string;
@@ -38,6 +48,27 @@ function Navbar({ classname, ...rest }: NavbarProps) {
                     Register
                   </Button>
                 </Link>
+              </li>
+              <li className="flex items-center">
+                <DropdownMenu modal={false}>
+                  <DropdownMenuTrigger>
+                    <Avatar>
+                      <AvatarImage src="https://github.com/unknown.png" />
+                      <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent align="end">
+                    <DropdownMenuLabel>
+                      <p>John Doe</p>
+                      <p className="text-gray-500 text-xs">johndoe@gmail.com</p>
+                    </DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem className="space-x-1">
+                      <LogOut className="h-4 w-4 text-gray-600" />
+                      <span>Logout</span>
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
               </li>
             </ul>
           </div>
