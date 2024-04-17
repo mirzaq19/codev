f41658513b2c278542adad1d7259416d7c85cfa6
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2024-04-17 16:16:15 +0700

    fix: typo filename

---
+++ src/types/comment.d.ts
@@ -0,0 +1,9 @@
+import { User } from '@/types/auth';
+
+export type Comment = {
+  id: string;
+  content: string;
+  createdAt: string;
+  upVotesBy: string[];
+  downVotesBy: string[];
+} & { owner: Omit<User, 'email'> };
