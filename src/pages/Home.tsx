33a7ec3feadf8fe50ad4961fd589e469a016f86b
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2024-04-17 08:00:33 +0700

    clean: remove unused code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,44 +4,6 @@ import ThreadList, { ThreadWithOwner } from '@/components/layout/ThreadList';
 import CategoryList from '@/components/layout/CategoryList';
 import { asyncPopulateUsersAndThreads } from '@/services/states/share-thunk';
 
-// const categories: string[] = ['React', 'Vue', 'Angular'];
-
-// const cards: CardProps[] = [
-//   {
-//     id: '1',
-//     title: 'How to use React Query',
-//     body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-//     category: 'React',
-//     ownerId: '1',
-//     upVotesBy: ['1', '2'],
-//     downVotesBy: ['3'],
-//     totalComments: 5,
-//     createdAt: '2021-10-10',
-//   },
-//   {
-//     id: '2',
-//     title: 'How to use Vue Query',
-//     body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-//     category: 'Vue',
-//     ownerId: '1',
-//     upVotesBy: ['1', '2'],
-//     downVotesBy: ['3'],
-//     totalComments: 5,
-//     createdAt: '2021-10-10',
-//   },
-//   {
-//     id: '3',
-//     title: 'How to use Angular Query',
-//     body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-//     category: 'Angular',
-//     ownerId: '1',
-//     upVotesBy: ['1', '2'],
-//     downVotesBy: ['3'],
-//     totalComments: 5,
-//     createdAt: '2021-10-10',
-//   },
-// ];
-
 function Home() {
   const [categories, setCategories] = useState<string[]>([]);
   const dispatch = useAppDispatch();
@@ -64,8 +26,6 @@ function Home() {
       owner: user,
     };
   }) as ThreadWithOwner[];
-
-  console.log('threadList', threads);
 
   return (
     <div className="min-h-main mt-4 px-4 lg:px-0">
