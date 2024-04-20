14aff3278355fd93f844c9811830644316b9d643
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2024-04-20 12:03:51 +0700

    feat: add classname prop in container component

--- src/components/layout/Container.tsx
+++ src/components/layout/Container.tsx
@@ -1,12 +1,13 @@
 import { cn } from '@/lib/utils';
 
 type ContainerProps = {
+  className?: string;
   children: React.ReactNode;
 } & React.HTMLAttributes<HTMLDivElement>;
 
-function Container({ children, ...rest }: ContainerProps) {
+function Container({ children, className, ...rest }: ContainerProps) {
   return (
-    <div className={cn('w-full lg:max-w-5xl mx-auto')} {...rest}>
+    <div className={cn('w-full lg:max-w-5xl mx-auto', className)} {...rest}>
       {children}
     </div>
   );
