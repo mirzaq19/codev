6859dc214982f914264c36623c310d19d050b6c0
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2024-04-26 10:44:45 +0700

    refactor: separate login input from login page into component

---
+++ src/components/content/LoginInput.tsx
@@ -0,0 +1,116 @@
+import { useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Link, useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { Loader2 } from 'lucide-react';
+import codevlogo from '@/assets/logo/codev-logo.svg';
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+
+type LoginInputProps = {
+  handleLogin: (email: string, password: string) => Promise<boolean>;
+};
+
+const formSchema = z.object({
+  email: z
+    .string()
+    .min(1, {
+      message: 'You must enter an email address.',
+    })
+    .email({
+      message: 'Please enter a valid email address',
+    }),
+  password: z.string().min(1, {
+    message: 'You must enter a password.',
+  }),
+});
+
+function LoginInput({ handleLogin }: LoginInputProps) {
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    setLoading(true);
+    const { email, password } = values;
+    const status = await handleLogin(email, password);
+    setLoading(false);
+    if (status) navigate('/');
+  };
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center gap-3">
+        <img
+          src={codevlogo}
+          alt="Codev logo"
+          className="w-8 h-8 md:w-12 md:h-12"
+        />
+        <h1 className="font-title font-bold text-center mb-6">Co-Dev Login</h1>
+      </div>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-4 w-full"
+        >
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input type="email" placeholder="email" {...field} />
+                </FormControl>
+                <FormMessage className="font-normal text-sm" />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input type="password" placeholder="password" {...field} />
+                </FormControl>
+                <FormMessage className="font-normal text-sm" />
+              </FormItem>
+            )}
+          />
+          <p className="text-sm text-muted-foreground">
+            Don&apos;t have an account?{' '}
+            <Link
+              className="text-foreground font-semibold hover:underline"
+              to="/register"
+            >
+              Register
+            </Link>
+          </p>
+          <Button disabled={loading} className="w-full" type="submit">
+            {loading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+            <span>Login</span>
+          </Button>
+        </form>
+      </Form>
+    </>
+  );
+}
+export default LoginInput;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,57 +1,14 @@
-import { useState } from 'react';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form';
-import { z } from 'zod';
-import { Link, useNavigate } from 'react-router-dom';
-import { Loader2 } from 'lucide-react';
-import { Button } from '@/components/ui/button';
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '@/components/ui/form';
-import { Input } from '@/components/ui/input';
 import { cn } from '@/lib/utils';
-import codevlogo from '@/assets/logo/codev-logo.svg';
 import { asyncLoginUser } from '@/services/states/auth-slice';
 import { useAppDispatch } from '@/app/hooks';
-
-const formSchema = z.object({
-  email: z
-    .string()
-    .min(1, {
-      message: 'You must enter an email address.',
-    })
-    .email({
-      message: 'Please enter a valid email address',
-    }),
-  password: z.string().min(1, {
-    message: 'You must enter a password.',
-  }),
-});
+import LoginInput from '@/components/content/LoginInput';
 
 function Login() {
-  const [loading, setLoading] = useState(false);
   const dispatch = useAppDispatch();
-  const navigate = useNavigate();
 
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      email: '',
-      password: '',
-    },
-  });
-
-  const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    setLoading(true);
-    const { email, password } = values;
+  const handleLogin = async (email: string, password: string) => {
     const status = await dispatch(asyncLoginUser({ email, password }));
-    setLoading(false);
-    if (status) navigate('/');
+    return status;
   };
 
   return (
@@ -59,62 +16,7 @@ function Login() {
       <div
         className={cn('max-w-3xl w-full mx-auto', 'bg-white rounded-md p-8')}
       >
-        <div className="flex flex-col items-center justify-center gap-3">
-          <img
-            src={codevlogo}
-            alt="Codev logo"
-            className="w-8 h-8 md:w-12 md:h-12"
-          />
-          <h1 className="font-title font-bold text-center mb-6">
-            Co-Dev Login
-          </h1>
-        </div>
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className="space-y-4 w-full"
-          >
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Email</FormLabel>
-                  <FormControl>
-                    <Input type="email" placeholder="email" {...field} />
-                  </FormControl>
-                  <FormMessage className="font-normal text-sm" />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <Input type="password" placeholder="password" {...field} />
-                  </FormControl>
-                  <FormMessage className="font-normal text-sm" />
-                </FormItem>
-              )}
-            />
-            <p className="text-sm text-muted-foreground">
-              Don&apos;t have an account?{' '}
-              <Link
-                className="text-foreground font-semibold hover:underline"
-                to="/register"
-              >
-                Register
-              </Link>
-            </p>
-            <Button disabled={loading} className="w-full" type="submit">
-              {loading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
-              <span>Login</span>
-            </Button>
-          </form>
-        </Form>
+        <LoginInput handleLogin={handleLogin} />
       </div>
     </div>
   );
