f48e66a31bbe933718983995e32f5ab8613a9dd4
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2024-04-27 21:26:40 +0700

    fix: add role to thread item component

--- src/components/content/ThreadItem.tsx
+++ src/components/content/ThreadItem.tsx
@@ -74,79 +74,75 @@ function ThreadItem({ thread, className, ...rest }: ThreadItemProps) {
   };
 
   return (
-    <div className={className} {...rest}>
-      <Card key={thread.id}>
-        <CardHeader className="pb-2">
-          <div className="flex gap-3">
-            <Avatar>
-              <AvatarImage src={thread.owner.avatar} />
-              <AvatarFallback>{thread.owner.name[0]}</AvatarFallback>
-            </Avatar>
-            <div>
-              <h3 className="text-base md:text-lg font-bold">
-                {thread.owner.name}
-              </h3>
-              <span className="text-gray-500 text-sm">
-                {thread.owner.email}
-              </span>
+    <Card role="listitem" className={className} {...rest}>
+      <CardHeader className="pb-2">
+        <div className="flex gap-3">
+          <Avatar>
+            <AvatarImage src={thread.owner.avatar} />
+            <AvatarFallback>{thread.owner.name[0]}</AvatarFallback>
+          </Avatar>
+          <div>
+            <h3 className="text-base md:text-lg font-bold">
+              {thread.owner.name}
+            </h3>
+            <span className="text-gray-500 text-sm">{thread.owner.email}</span>
+          </div>
+        </div>
+        <div className="flex flex-col gap-2 md:flex-row justify-between">
+          <div>
+            <Link to={`/thread/${thread.id}`}>
+              <CardTitle className="line-clamp-2 hover:underline">
+                {thread.title}
+              </CardTitle>
+            </Link>
+            <div className="bg-green-50 text-gray-600 rounded mt-2 w-fit px-2 hover:underline">
+              #{thread.category}
             </div>
           </div>
-          <div className="flex flex-col gap-2 md:flex-row justify-between">
-            <div>
-              <Link to={`/thread/${thread.id}`}>
-                <CardTitle className="line-clamp-2 hover:underline">
-                  {thread.title}
-                </CardTitle>
-              </Link>
-              <div className="bg-green-50 text-gray-600 rounded mt-2 w-fit px-2 hover:underline">
-                #{thread.category}
-              </div>
-            </div>
-            <CardDescription>{timeDiff(thread.createdAt)}</CardDescription>
-          </div>
-        </CardHeader>
-        <CardContent className="line-clamp-4 leading-snug break-words">
-          <div>{parse(thread.body)}</div>
-        </CardContent>
-        <CardFooter className="flex justify-between">
-          <div className="md:space-x-2">
-            <Button
-              onClick={() => onUpVote(thread.id)}
-              variant="ghost"
-              className={`space-x-2 ${upvoted ? 'bg-green-50' : ''}`}
-            >
-              <ThumbsUp className="w-5 h-5" />
-              <p className="text-sm">
-                {thread.upVotesBy.length}{' '}
-                <span className="hidden md:inline">Upvotes</span>
-              </p>
-            </Button>
-            <Button
-              onClick={() => onDownVote(thread.id)}
-              variant="ghost"
-              className={`space-x-2 ${downvoted ? 'bg-red-50' : ''}`}
-            >
-              <ThumbsDown className="w-5 h-5" />
-              <p>
-                {thread.downVotesBy.length}{' '}
-                <span className="hidden md:inline">Downvotes</span>
-              </p>
-            </Button>
-          </div>
+          <CardDescription>{timeDiff(thread.createdAt)}</CardDescription>
+        </div>
+      </CardHeader>
+      <CardContent className="line-clamp-4 leading-snug break-words">
+        <div>{parse(thread.body)}</div>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <div className="md:space-x-2">
           <Button
-            to={`/thread/${thread.id}`}
+            onClick={() => onUpVote(thread.id)}
             variant="ghost"
-            className="space-x-2"
+            className={`space-x-2 ${upvoted ? 'bg-green-50' : ''}`}
           >
-            <MessageCircleMore className="w-5 h-5" />
-            <p>
-              {thread.totalComments}{' '}
-              <span className="hidden md:inline">Comments</span>
+            <ThumbsUp className="w-5 h-5" />
+            <p className="text-sm">
+              {thread.upVotesBy.length}{' '}
+              <span className="hidden md:inline">Upvotes</span>
             </p>
           </Button>
-        </CardFooter>
-      </Card>
-    </div>
+          <Button
+            onClick={() => onDownVote(thread.id)}
+            variant="ghost"
+            className={`space-x-2 ${downvoted ? 'bg-red-50' : ''}`}
+          >
+            <ThumbsDown className="w-5 h-5" />
+            <p>
+              {thread.downVotesBy.length}{' '}
+              <span className="hidden md:inline">Downvotes</span>
+            </p>
+          </Button>
+        </div>
+        <Button
+          to={`/thread/${thread.id}`}
+          variant="ghost"
+          className="space-x-2"
+        >
+          <MessageCircleMore className="w-5 h-5" />
+          <p>
+            {thread.totalComments}{' '}
+            <span className="hidden md:inline">Comments</span>
+          </p>
+        </Button>
+      </CardFooter>
+    </Card>
   );
 }
 
