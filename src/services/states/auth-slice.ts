d9a8b3eacbc5465e003fb2d453171d4e7d81e547
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2024-04-28 10:15:28 +0700

    test: add some error mistake

--- src/services/states/auth-slice.ts
+++ src/services/states/auth-slice.ts
@@ -33,7 +33,7 @@ export const authSlice = createSlice({
       state.authenticated = false;
     },
     stopLoading: (state) => {
-      state.loading = false;
+      state.loading = true;
     },
   },
 });
