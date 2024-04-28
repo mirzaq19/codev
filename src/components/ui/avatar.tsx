8001beff3af9eb6fa645ad23938517359550ee99
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2024-04-28 09:18:13 +0700

    feat: add testid to avatar component

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -9,6 +9,7 @@ const Avatar = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <AvatarPrimitive.Root
     ref={ref}
+    data-testid="avatar"
     className={cn(
       'relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full',
       className,
